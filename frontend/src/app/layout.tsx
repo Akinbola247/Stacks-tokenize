c06185f82a6aeb0ecd10a398eb37cb10e0c6da48
Akinbola247 <akinbolakehinde247@gmail.com>
2026-05-17 11:44:18 +0100

    changes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="bg-[#131416]">
+      <head>
+        <meta
+          name="talentapp:project_verification"
+          content="8982e2cc9b6ff40bea4745719d2b1e9e4886cb2ef08d9e37ae6ce705a48562e67d175fd2342ead5bd3b98e2992fc8f4c126636eb805e76ebb2055dafd1fe62d2"
+        />
+      </head>
       <body className="bg-[#131416] text-white">
         <WalletProvider>
           <Header />
